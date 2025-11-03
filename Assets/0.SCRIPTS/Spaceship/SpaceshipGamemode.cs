cd7d30d9c6783995d46e0b84983f853f76ebef6d
Zorro Svärdedahl <svardendahl.zorro@gmail.com>
2025-11-03 17:21:56 +0100

    Work on Zorro.Framework

---
+++ Assets/0.SCRIPTS/Spaceship/SpaceshipGamemode.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using Zorro.Framework;
+
+namespace Zorro.TestGame.Spaceship
+{
+    public class SpaceshipGamemode : Gamemode
+    {
+        public override void PlayerLoadedLevel(Player player)
+        {
+            base.PlayerLoadedLevel(player);
+            
+            Debug.Log($"PlayerLoadedLevel: {player}");
+            
+            Instantiate(Resources.Load<GameObject>("Spaceship"));
+        }
+    }
+}
--- Assets/0.SCRIPTS/TestGameGameSettings.cs
+++ Assets/0.SCRIPTS/TestGameGameSettings.cs
@@ -1,11 +1,18 @@
-﻿using UnityEngine;
+﻿using Photon.Realtime;
+using UnityEngine;
 using Zorro.Framework;
+using Zorro.Framework.Photon;
 
 namespace Zorro.TestGame
 {
     [CreateAssetMenu(fileName = nameof(GameSettings), menuName = "TestGame/" + nameof(GameSettings))]
-    public class TestGameGameSettings : GameSettings
+    public class TestGameGameSettings : GameSettings, IPhotonAppSettingHolder
     {
+        public AppSettings PhotonAppSettings;
         
+        public AppSettings GetAppSettings()
+        {
+            return PhotonAppSettings;
+        }
     }
 }
